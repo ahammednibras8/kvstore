92e2e4c6768ce42b13a2d81e11270e5c262b9ff6
ahammednibras8 <ahammednibras@proton.me>
2025-11-17 19:44:32 +0530

    Added SET and GET initial implementation

--- main.go
+++ main.go
@@ -9,6 +9,28 @@ type KVStore struct {
 	data map[string]string
 }
 
-func main() {
+func NewStore() *KVStore {
+	return &KVStore{
+		data: make(map[string]string),
+	}
+}
 
+func (s *KVStore) Set(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.data[key] = value
+}
+
+func (s *KVStore) Get(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	value, ok := s.data[key]
+	return value, ok
+}
+
+func main() {
+	store := NewStore()
+	_ = store
 }
