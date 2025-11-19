505b800ea29e4d0ebc881bc9373f31a33d796d2a
ahammednibras8 <ahammednibras@proton.me>
2025-11-19 11:42:54 +0530

    Added Wal.log Open

--- wal/wal.go
+++ wal/wal.go
@@ -11,3 +11,24 @@ type WAL struct {
 	path   string
 	offset int64
 }
+
+func Open(path string) (*WAL, error) {
+	flags := os.O_CREATE | os.O_RDWR | os.O_APPEND
+
+	f, err := os.OpenFile(path, flags, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := f.Stat()
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+
+	return &WAL{
+		file:   f,
+		path:   path,
+		offset: info.Size(),
+	}, nil
+}
