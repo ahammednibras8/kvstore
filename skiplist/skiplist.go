3a1d6d9314b8a26b1b6d6403da849cda173ae5c9
ahammednibras8 <ahammednibras@proton.me>
2025-11-19 09:35:54 +0530

    Now we need to implement the logic that makes a Skip List organic. Unlike a balanced tree that strictly enforces structure, a Skip List relies on probability to decide how tall a new node should be.

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,10 +1,16 @@
 package skiplist
 
+import (
+	"math/rand"
+	"time"
+)
+
 type SkipList struct {
 	Head     *Node
 	MaxLevel int
 	Level    int
 	P        float64
+	rng      *rand.Rand
 }
 
 func NewSkipList(p float64, maxLevel int) *SkipList {
@@ -14,10 +20,24 @@ func NewSkipList(p float64, maxLevel int) *SkipList {
 		Next:  make([]*Node, maxLevel),
 	}
 
+	src := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(src)
+
 	return &SkipList{
 		Head:     head,
 		MaxLevel: maxLevel,
 		Level:    1,
 		P:        p,
+		rng:      rng,
 	}
+}
+
+func (s *SkipList) randomLevel() int {
+	level := 1
+
+	for s.rng.Float64() < s.P && level < s.MaxLevel {
+		level++
+	}
+
+	return level
 }
